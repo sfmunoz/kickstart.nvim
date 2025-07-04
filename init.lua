08c23e528bb0494edbbf9c6fd4093222e4b086d5
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-07-04 14:38:14 +0000

    init.lua: 'ruby_lsp' (Ruby) LSP enabled

--- init.lua
+++ init.lua
@@ -625,6 +625,7 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         pyright = {},
+        ruby_lsp = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
